2ca4267f616ee0849cfaf517de7462b573f4c094
Dave Barker <david@dbark.co.uk>
2016-05-13 23:12:01 +0100

    Restructured page render functions and updated tests

--- bsw/pages.py
+++ bsw/pages.py
@@ -51,9 +51,9 @@ class Page:
         for match in matches:
             include_data = includes.get_include(match)
             regex_this_include = "<!--\s+include\(\"{0}\"\)\s+-->".format(match)
-            self.rendered_page = re.sub(regex_this_include,
-                                        include_data,
-                                        self.body)
+            self.body = re.sub(regex_this_include,
+                               include_data,
+                               self.body)
 
     def render(self):
         """Render page using template, insert any includes and do page
@@ -64,8 +64,8 @@ class Page:
         else:
             template = templates.get_template("base.html")
 
-        self.rendered_page = template.replace("$page_content", self.body)
         self.replace_includes()
+        self.rendered_page = template.replace("$page_content", self.body)
         for page_var in self.page_vars:
             self.rendered_page = self.rendered_page.replace("$" + page_var,
                                                             self.page_vars[page_var])
--- tests/test_pages.py
+++ tests/test_pages.py
@@ -7,15 +7,17 @@ import bsw.includes
 
 class TestPages(unittest.TestCase):
     def setUp(self):
-        self.test_page_filename = os.path.join(os.path.dirname(__file__),
-                                               "data",
+        self.data_base_dir = os.path.join(os.path.dirname(__file__), "data")
+        self.test_page_filename = os.path.join(self.data_base_dir,
                                                "pages",
                                                "test_page.html")
-        self.test_include_path = os.path.join(os.path.dirname(__file__),
-                                              "data",
+        self.test_include_path = os.path.join(self.data_base_dir,
                                               "templates",
                                               "includes",
                                               "test_include.html")
+        self.test_template_path = os.path.join(self.data_base_dir,
+                                               "templates",
+                                               "test_template.html")
 
     def test_page_init(self):
         page = Page(self.test_page_filename)
@@ -57,11 +59,34 @@ class TestPages(unittest.TestCase):
         bsw.includes.include_cache["test_include.html"] = test_include_data
 
         test_page = Page(self.test_page_filename)
-        test_page.load()
-        test_page.extract_vars()
-        test_page.strip_vars()
+        test_page.load_and_parse()
+
         test_page.replace_includes()
         self.assertNotIn("<!-- include(\"test_include.html\") -->",
-                         test_page.rendered_page)
+                         test_page.body)
         self.assertIn("<h1>This is a test include</h1>",
+                      test_page.body)
+
+    def test_render(self):
+        # Preload the includes cache
+        with open(self.test_include_path, "r") as test_include_file:
+            test_include_data = test_include_file.read()
+        bsw.includes.include_cache["test_include.html"] = test_include_data
+
+        # Preload the templates cache
+        with open(self.test_template_path, "r") as test_template_file:
+            test_template_data = test_template_file.read()
+        bsw.templates.template_cache["test_template.html"] = test_template_data
+
+        test_page = Page(self.test_page_filename)
+        test_page.load_and_parse()
+
+        test_page.render()
+        self.assertIn("My sample page",
+                      test_page.rendered_page)
+        self.assertIn("<h1>This is a test include</h1>",
+                      test_page.rendered_page)
+        self.assertIn("<h1>Template Header</h1>",
+                      test_page.rendered_page)
+        self.assertIn("<h1>Template Footer</h1>",
                       test_page.rendered_page)
