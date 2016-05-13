66adbee0402239a113ff4076321382c46c895524
Dave Barker <david@dbark.co.uk>
2016-05-13 22:44:49 +0100

    Added some more page unit tests

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -2,13 +2,20 @@ import os
 import unittest
 
 from bsw.pages import Page
+import bsw.includes
 
 
 class TestPages(unittest.TestCase):
-    test_page_filename = os.path.join(os.path.dirname(__file__),
-                                      "data",
-                                      "pages",
-                                      "test_page.html")
+    def setUp(self):
+        self.test_page_filename = os.path.join(os.path.dirname(__file__),
+                                               "data",
+                                               "pages",
+                                               "test_page.html")
+        self.test_include_path = os.path.join(os.path.dirname(__file__),
+                                              "data",
+                                              "templates",
+                                              "includes",
+                                              "test_include.html")
 
     def test_page_init(self):
         page = Page(self.test_page_filename)
@@ -41,3 +48,20 @@ class TestPages(unittest.TestCase):
         test_page.strip_vars()
         self.assertNotIn("<!-- template = \"test_template.htm\" -->",
                          test_page.body)
+
+    def test_replace_includes(self):
+        # Preload the includes cache
+        with open(self.test_include_path, "r") as test_include_file:
+            test_include_data = test_include_file.read()
+
+        bsw.includes.include_cache["test_include.html"] = test_include_data
+
+        test_page = Page(self.test_page_filename)
+        test_page.load()
+        test_page.extract_vars()
+        test_page.strip_vars()
+        test_page.replace_includes()
+        self.assertNotIn("<!-- include(\"test_include.html\") -->",
+                         test_page.rendered_page)
+        self.assertIn("<h1>This is a test include</h1>",
+                      test_page.rendered_page)
